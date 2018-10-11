3cbacf82ba4dfd0ba1a1860ca2e3cce1de2a3d69
amieguo <amieguo@gmail.com>
2018-10-10 17:31:04 -0700

    working on connecting to server

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,33 @@
 var db = require('../db');
+var connection = require('../db/index.js');
+
+connection.connection();
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (request, response) {
+      connection.query('SELECT * FROM Messages', function (err, results) {
+      if (err) {
+        throw err;
+      } else {
+        response.send(JSON.stringify(results));
+        
+        console.log('The solution is: ')
+      }
+      }) 
+    }, // a function which produces all the messages
+    post: function (request, response) {
+      connection.query('INSERT INTO `Messages` (Message) VALUE (?)'), function(err, message) {
+        if (err) {
+        throw err;
+      } else {
+        message = JSON.parse(message);
+        response.end();
+        console.log('The solution is: ')
+      }
+      
+      
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,94 @@
 CREATE DATABASE chat;
 
 USE chat;
+-- ---
+-- Globals
+-- ---
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `userName` CHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
+-- ---
+-- Table 'Rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+    
+CREATE TABLE `Rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `roomName` CHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `Message` MEDIUMTEXT NULL DEFAULT NULL,
+  `id_Users` INTEGER NULL DEFAULT NULL,
+  `id_Rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Friends'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Friends`;
+    
+CREATE TABLE `Friends` (
+  `id_Users` INTEGER NULL DEFAULT NULL
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Rooms) REFERENCES `Rooms` (`id`);
+ALTER TABLE `Friends` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Friends` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Users` (`id`,`userName`) VALUES
+-- ('','');
+-- INSERT INTO `Rooms` (`id`,`roomName`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`Message`,`id_Users`,`id_Rooms`) VALUES
+-- ('','','','');
+-- INSERT INTO `Friends` (`id_Users`) VALUES
+-- ('');
 /* Create other tables and define schemas for them here! */
 
 
